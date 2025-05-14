9b79b98ae9e0b5dc250c3a43d3dae03695017810
israelr9897 <admin@admins-MacBook-Pro.local>
2025-05-14 17:19:20 +0300

    j

--- atbash/obj/Debug/net8.0/atbash.AssemblyInfo.cs
+++ atbash/obj/Debug/net8.0/atbash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atbash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd257a576f1cb71bf11860afb4d71e844a62449")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f7cf92049d454e35ab53e2ec169d3b05b1eb72")]
 [assembly: System.Reflection.AssemblyProductAttribute("atbash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atbash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
