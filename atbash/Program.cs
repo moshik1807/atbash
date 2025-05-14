a73ffd561e77cd2794cf9d5944dc961cfb2774cd
Dan <danhemed123@gmail.com>
2025-05-14 15:25:03 +0300

    Building the base of the printMessage function

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("dan");
-Console.WriteLine("dan");
+﻿void printMessage(int point, string message)
+{
+
+}
