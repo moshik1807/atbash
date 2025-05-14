cc7dbedbc98bc8a5cc562f1dc29ae8fea0990aae
moshik <moshik0527669169@gmail.com>
2025-05-14 16:03:42 +0300

    endSearch

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -1,26 +1,39 @@
 
 // See https://aka.ms/new-console-template for more information
 
-string CipherKey(string EncryptedText)
+//string CipherKey(string EncryptedText)
+//{
+
+//}
+
+//char DecodingCode(char signal)
+//{
+
+//}
+
+
+//﻿void printMessage(int point, string message)
+//{
+
+//}bomb, nukhba, fighter, rocket, secret
+
+
+
+using System.Diagnostics.Tracing;
+
+int SearchProblemticWords(string LisztDeciphered)
 {
-
+    int counter = 0;
+    string str = "bomb, nukhba, fighter, rocket, secret";
+    char[] delimiterChars = [' ',',',':','-','.','/','!','?'];
+    string[] WordsList = LisztDeciphered.Split(delimiterChars);
+    foreach (var word in WordsList)
+    {
+        if (str.Contains(word))
+        {
+            counter += 1;
+        }
+    }
+    return counter;
 }
-
-char DecodingCode(char signal)
-{
-    
-}
-
-
-﻿void printMessage(int point, string message)
-{
-
-}
-
-
-﻿int SearchProblemticWords(string LisztDeciphered)
-{
-    char[] delimiterChars = [' ',',',':','-','.','\t'];
-    string [] WordsList = LisztDeciphered.Split(delimiterChars);
-       
-}
+Console.WriteLine("");
